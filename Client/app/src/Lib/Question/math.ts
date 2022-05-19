58a35b7eaa18b8536bc68f2900a6557a3939c75a
theArctesian <stephen.d.okita@gmail.com>
2022-05-19 10:25:25 +0800

    rational sucide should not be taboo

--- Client/app/src/Lib/Question/math.ts
+++ Client/app/src/Lib/Question/math.ts
@@ -1,25 +1,42 @@
-export const allQuestions : string[] = [
-
-]
+// TODO: Josh write file paths into array
 
 export const functions : string[] = [
 
 ]
 
+export const functionsAns : string[] = [
+
+]
+
+
 export const geoTrig : string[] = [
 
 ]
+export const geoTrigAns : string[] = [
 
+]
 export const numAlg : string[] = [
 
 ]
+export const numAlgAns : string[] = [
+
+]
+
 
 export const statProp : string[] = [
 
 ]
 
+export const statPropAns : string[] = [
 
+]
 
+export const allQuestions : string[] = [
 
+]
+
+export const allQuestionsAns : string[] = [
+
+]
 
 
