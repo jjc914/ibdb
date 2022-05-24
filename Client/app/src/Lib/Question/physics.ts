a237aeb2359f222b0b1a63140cf9811a88d21db6
theArctesian <stephen.d.okita@gmail.com>
2022-05-24 19:39:32 +0800

    sucide, if rationally thought out, is a pretty good idea

--- Client/app/src/Lib/Question/physics.ts
+++ Client/app/src/Lib/Question/physics.ts
@@ -1,52 +1,123 @@
-export const link : string = ""
+/** To all who have to subject themselves to this code: 
+    * Do not fret, I am perfectly aware of how atrocious of a DB it is. I am almost embarrassed to put my name on this:
+        * Is it scalable : no
+        * Is it the most effective solution: no
+        * Is it the fastest solution: probs not but we are rather pressed on time so this was the fastest cheapest solutions  
+    * if this actually gets put into production I will put it on supdabase but for now you will have to put up with this
+2022, May 
+the Arctesian
+ */
 
-export const allQuestions : string[] = [
+export const physicsConstLink : string = "https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/"
 
+export const allPhysicsQuestions : string[] = [
+    'atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object10answerC.pngquestion13answerC.png',
+    'atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object17answerA.pngquestion38answerA.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object1answerB.pngquestion24answerB.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object9answerC.pngquestion25answerC.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object2answerC.pngquestion28answerC.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object14answerD.pngquestion31answerD.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object1answerA.pngquestion30answerA.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage9object1answerA.pngquestion11answerA.png',
+    'electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object11answerB.pngquestion17answerB.png',
+    "electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object19answerC.pngquestion29answerC.png",
+    "electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage20object10answerB.pngquestion33answerB.png",
+    "energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage12object10answerC.pngquestion20answerC.png",
+    "energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage17object1answerA.pngquestion27answerA.png",
+    "energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object6answerB.pngquestion37answerB.png",
+    "energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage5object1answerB.pngquestion5answerB.png",
+    "fields/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object16answerD.pngquestion18answerD.png",
+    "fields/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object1answerC.pngquestion36answerC.png",
+    "fields/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage20object2answerC.pngquestion32answerC.png",
+    "measurementsAnduncertainty/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage21object7answerB.pngquestion35answerB.png",
+    "measurementsAnduncertainty/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage2object2answerA.pngquestion0answerA.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object2answerD.pngquestion12answerD.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage3object11answerD.pngquestion3answerD.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage16object2answerB.pngquestion26answerB.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage3object1answerA.pngquestion2answerA.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage2object15answerD.pngquestion1answerD.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage4object2answerC.pngquestion4answerC.png",
+    "quantumAndnuclearPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object15answerC.pngquestion14answerC.png",
+    "quantumAndnuclearPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object6answerD.pngquestion16answerD.png",
+    "quantumAndnuclearPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object1answerA.pngquestion15answerA.png",
+    "thermal/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage12object2answerA.pngquestion19answerA.png", 
+    "thermal/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage6object2answerB.pngquestion7answerB.png",
+    "thermal/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage5object10answerA.pngquestion6answerA.png",
+    "waves/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage7object9answerD.pngquestion9answerD.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage13object1answerB.pngquestion21answerB.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage21object1answerD.pngquestion34answerD.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage14object2answerA.pngquestion22answerA.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage7object1answerD.pngquestion8answerD.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage14object7answerD.pngquestion23answerD.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage8object2answerC.pngquestion10answerC.png",
 ]
-export const atomic : string[] = [
-'atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object10answerC.pngquestion13answerC.png',
-'atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object17answerA.pngquestion38answerA.png'
+export const atomicPhysics : string[] = [
+    'atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object10answerC.pngquestion13answerC.png',
+    'atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object17answerA.pngquestion38answerA.png'
 ]
 
 
-export const energyMag : string[] = [
-'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object1answerB.pngquestion24answerB.png',
-'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object9answerC.pngquestion25answerC.png',
-'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object2answerC.pngquestion28answerC.png',
-'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object14answerD.pngquestion31answerD.png',
-'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object1answerA.pngquestion30answerA.png',
-'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage9object1answerA.pngquestion11answerA.png'
+export const electricityAndmagnetism : string[] = [
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object1answerB.pngquestion24answerB.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object9answerC.pngquestion25answerC.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object2answerC.pngquestion28answerC.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object14answerD.pngquestion31answerD.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object1answerA.pngquestion30answerA.png',
+    'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage9object1answerA.pngquestion11answerA.png'
 ]
 
-export const electroInduction : string[] = [
-'electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object11answerB.pngquestion17answerB.png',
-"electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object19answerC.pngquestion29answerC.png",
-"electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage20object10answerB.pngquestion33answerB.png"
+export const electromagneticInduction : string[] = [
+    'electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object11answerB.pngquestion17answerB.png',
+    "electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object19answerC.pngquestion29answerC.png",
+    "electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage20object10answerB.pngquestion33answerB.png"
 ]
 
 
 export const energyProduction : string[] = [
-"energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage12object10answerC.pngquestion20answerC.png",
-"energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage17object1answerA.pngquestion27answerA.png",
-"energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object6answerB.pngquestion37answerB.png",
-"energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage5object1answerB.pngquestion5answerB.png"
+    "energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage12object10answerC.pngquestion20answerC.png",
+    "energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage17object1answerA.pngquestion27answerA.png",
+    "energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object6answerB.pngquestion37answerB.png",
+    "energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage5object1answerB.pngquestion5answerB.png"
 ]
 
+export const fields : string[] = [
+    "fields/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object16answerD.pngquestion18answerD.png",
+    "fields/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object1answerC.pngquestion36answerC.png",
+    "fields/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage20object2answerC.pngquestion32answerC.png"
+]
 export const measurments : string[] = [
+    "measurementsAnduncertainty/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage21object7answerB.pngquestion35answerB.png",
+    "measurementsAnduncertainty/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage2object2answerA.pngquestion0answerA.png"
+]
 
+export const mechanics : string[] = [
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object2answerD.pngquestion12answerD.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage3object11answerD.pngquestion3answerD.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage16object2answerB.pngquestion26answerB.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage3object1answerA.pngquestion2answerA.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage2object15answerD.pngquestion1answerD.png",
+    "mechanics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage4object2answerC.pngquestion4answerC.png"
 ]
 
 export const quantumNuclear : string[] = [
-
+    "quantumAndnuclearPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object15answerC.pngquestion14answerC.png",
+    "quantumAndnuclearPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object6answerD.pngquestion16answerD.png",
+    "quantumAndnuclearPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object1answerA.pngquestion15answerA.png"
 ]
-export const mechanics : string[] = [
 
-]
 export const thermal : string[] = [
-
+    "thermal/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage12object2answerA.pngquestion19answerA.png", 
+    "thermal/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage6object2answerB.pngquestion7answerB.png",
+    "thermal/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage5object10answerA.pngquestion6answerA.png"
 ]
 export const waves : string[] = [
-
+    "waves/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage7object9answerD.pngquestion9answerD.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage13object1answerB.pngquestion21answerB.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage21object1answerD.pngquestion34answerD.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage14object2answerA.pngquestion22answerA.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage7object1answerD.pngquestion8answerD.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage14object7answerD.pngquestion23answerD.png",
+    "wavePhenomena/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage8object2answerC.pngquestion10answerC.png"
 ]
 
 
