d2f8fc225315eff05de0f20ea64e3692ca7234e2
theArctesian <stephen.d.okita@gmail.com>
2022-05-24 19:15:29 +0800

    updates

--- Client/app/src/Lib/Question/physics.ts
+++ Client/app/src/Lib/Question/physics.ts
@@ -1,29 +1,36 @@
+export const link : string = ""
+
 export const allQuestions : string[] = [
 
 ]
 export const atomic : string[] = [
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object10answerC.pngquestion13answerC.png',
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object17answerA.pngquestion38answerA.png'
+'atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage10object10answerC.pngquestion13answerC.png',
+'atomicPhysics/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object17answerA.pngquestion38answerA.png'
 ]
 
-export const circMotion : string[] = [
 
-]
 export const energyMag : string[] = [
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object1answerB.pngquestion24answerB.png',
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object9answerC.pngquestion25answerC.png',
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object2answerC.pngquestion28answerC.png',
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object14answerD.pngquestion31answerD.png',
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object1answerA.pngquestion30answerA.png',
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage9object1answerA.pngquestion11answerA.png'
+'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object1answerB.pngquestion24answerB.png',
+'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage15object9answerC.pngquestion25answerC.png',
+'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object2answerC.pngquestion28answerC.png',
+'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object14answerD.pngquestion31answerD.png',
+'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage19object1answerA.pngquestion30answerA.png',
+'electricityAndmagnetism/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage9object1answerA.pngquestion11answerA.png'
 ]
 
 export const electroInduction : string[] = [
-'https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object11answerB.pngquestion17answerB.png',
-"https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object19answerC.pngquestion29answerC.png",
-"https://raw.githubusercontent.com/jjc914/ibdb/main/Server/DataProcessing/res/mcq/qs/out/phys/electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage20object10answerB.pngquestion33answerB.png",
+'electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage11object11answerB.pngquestion17answerB.png',
+"electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage18object19answerC.pngquestion29answerC.png",
+"electromagneticInduction/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage20object10answerB.pngquestion33answerB.png"
 ]
 
+
+export const energyProduction : string[] = [
+"energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage12object10answerC.pngquestion20answerC.png",
+"energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage17object1answerA.pngquestion27answerA.png",
+"energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage22object6answerB.pngquestion37answerB.png",
+"energyProdution/ServerDataProcessingresmcqqsoutPHYSICS-QS-MCQ-2016-MAYpage5object1answerB.pngquestion5answerB.png"
+]
 
 export const measurments : string[] = [
 
