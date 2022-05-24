c6dce78ec381e0377baa6656f90d39fe21e6d32a
Joshua Chasnov <48508979+jjc914@users.noreply.github.com>
2022-05-24 13:06:42 +0800

    Update math.ts

--- Client/app/src/Lib/Question/math.ts
+++ Client/app/src/Lib/Question/math.ts
@@ -1,75 +1,70 @@
 export const functions : string[] = [
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question3.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question5.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question6.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question7.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question8.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question9.png?raw=true"
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/func/10.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/func/2.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/func/4.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/func/8.png?raw=true"
 ]
 
 export const functionsAns : string[] = [
-  
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/10.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/2.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/4.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/8.png?raw=true"
 ]
 
-
 export const geoTrig : string[] = [
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question0.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question2.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question3.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question4.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question8.png?raw=true"
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/1.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/7.png?raw=true"
 ]
 
 export const geoTrigAns : string[] = [
-  
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/1.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/7.png?raw=true"
 ]
 
 export const numAlg : string[] = [
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question0.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question1.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question5.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question6.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question7.png?raw=true"
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/num&alg/5.png?raw=true"
 ]
 
 export const numAlgAns : string[] = [
-
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/5.png?raw=true"
 ]
 
 export const statProp : string[] = [
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/stat&prob/question1.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/stat&prob/question2.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/stat&prob/question9.png?raw=true"
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/3.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/6.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/9.png?raw=true"
 ]
 
 export const statPropAns : string[] = [
-
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/3.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/6.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/9.png?raw=true"
 ]
 
 export const allQuestions : string[] = [
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question3.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question5.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question6.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question7.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question8.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/func/question9.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question0.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question2.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question3.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question4.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/geo&trig/question8.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question0.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question1.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question5.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question6.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/num&alg/question7.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/stat&prob/question1.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/stat&prob/question2.png?raw=true",
-  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs/out/math/stat&prob/question9.png?raw=true"
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/func/10.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/func/2.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/func/4.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/func/8.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/1.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/7.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/num&alg/5.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/3.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/6.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/qs2/out/math/geo&trig/9.png?raw=true"
 ]
 
 export const allQuestionsAns : string[] = [
-
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/10.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/2.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/4.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/8.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/1.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/7.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/5.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/3.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/6.png?raw=true",
+  "https://github.com/jjc914/ibdb/blob/main/Server/DataProcessing/res/laq/ms2/9.png?raw=true"
+  
 ]
-
-
