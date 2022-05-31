d967f4b8e1c814dbdc2cc0bb66ec74c412ca31ad
theArctesian <stephen.d.okita@gmail.com>
2022-05-31 13:43:44 +0800

    go go pee pee

--- Client/app/src/Lib/functions/shuffle.ts
+++ Client/app/src/Lib/functions/shuffle.ts
@@ -1,8 +1,9 @@
-export const shuffle(array: any[]) {
+export const shuffle: (arg0: string[]) => string(array: any[]) => {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
+    return temp;
 }
--- Client/app/src/routes/game/physicsGame.svelte
+++ Client/app/src/routes/game/physicsGame.svelte
@@ -1,8 +1,109 @@
 <script lang="ts">
-  import test from '../../Lib/SVG/test.png'
-  import { correct, wrong } from '../../stores/authStore'
-  
+  import test from "../../Lib/SVG/test.png";
+  import { correct, wrong } from "../../stores/authStore";
+  import { allQuestions } from "../../Lib/Question/physics";
+  import { physicsConstLink } from "../../Lib/Question/physics";
+  import { onMount } from "svelte";
+
+  let question: string = "";
+  let ans: string = "";
+  onMount(async () => {
+    let index = Math.floor(Math.random() * allQuestions.length);
+    question = physicsConstLink + allQuestions[index];
+    console.log(question);
+    let temp = question.slice(-5);
+    ans = temp.charAt(0).toLocaleLowerCase();
+
+    console.log(ans);
+  });
+  function ansA() {
+    if (ans == "a") {
+      alert("Ans was right");
+      correct.update((n) => n + 1);
+    } else {
+      alert("Ans was wrong");
+      wrong.update((n) => n + 1);
+    }
+  }
+  function ansB() {
+    if (ans == "b") {
+      alert("Ans was right");
+      correct.update((n) => n + 1);
+    } else {
+      alert("Ans was wrong");
+      wrong.update((n) => n + 1);
+    }
+  }
+  function ansC() {
+    if (ans == "c") {
+      alert("Ans was right");
+      correct.update((n) => n + 1);
+    } else {
+      alert("Ans was wrong");
+      wrong.update((n) => n + 1);
+    }
+  }
+  function ansD() {
+    if (ans == "d") {
+      alert("Ans was right");
+      correct.update((n) => +1);
+    } else {
+      alert("Ans was wrong");
+      wrong.update((n) => +1);
+    }
+  }
 </script>
+
+<svelte:head>
+  <title>index</title>
+</svelte:head>
+
+<div class="flex justify-center items-center h-screen back">
+  <div class="score">
+    <h1>
+      <strong style="color: rgb(0,225,0);">Correct: </strong>{$correct}
+      <strong style="color: red;">Wrong: </strong>{$wrong}
+    </h1>
+  </div>
+  <div
+    class="flex flex-col border-2 backpeepee p-5 m-10 rounded-3xl drop-shadow-2xl"
+  >
+    <h1 class="text-2xl float-left">Question:</h1>
+    <br />
+    <img src={question} alt="question" class="rounded-xl question" />
+    <br />
+    <div
+      class="flex w-3/4 space-x-4 m-auto object-center text-center
+      content-center flex-row"
+    >
+      <div class="flex flex-col ansl">
+        <h1 class="text-xl text-center">A</h1>
+        <button on:click={ansA}>
+          <div class="ans" />
+        </button>
+      </div>
+      <div class="flex flex-col ansl">
+        <h1 class="text-xl text-center">B</h1>
+        <button on:click={ansB}>
+          <div class="ans" />
+        </button>
+      </div>
+      <div class="flex flex-col ansl">
+        <h1 class="text-xl text-center">C</h1>
+        <button on:click={ansC}>
+          <div class="ans" />
+        </button>
+      </div>
+      <div class="flex flex-col ansl">
+        <h1 class="text-xl text-center">D</h1>
+        <button on:click={ansD}>
+          <div class="ans" />
+        </button>
+      </div>
+    </div>
+    <br />
+  </div>
+</div>
 
 <style>
   .ans {
@@ -22,7 +123,7 @@
     background-color: #373737;
   }
   * {
-    font-family: 'Roboto', sans-serif;
+    font-family: "Roboto", sans-serif;
     color: white;
   }
   .ansl {
@@ -30,59 +131,12 @@
   }
   .question {
     width: 60vw;
-    height: 40vh;
+    max-height: 60vh;
   }
   .score {
     position: absolute;
     text-align: center;
     font-size: 2rem;
     top: 2rem;
-
   }
 </style>
-
-<svelte:head>
-  <title>index</title>
-</svelte:head>
-
-<div class="flex justify-center items-center h-screen back">
-<div class= "score">
-  <h1> <strong style="color: rgb(0,225,0);">Correct: </strong>{ $correct} <strong style="color: red;" >Wrong: </strong>{$wrong} </h1>
-</div>
-  <div
-    class="flex flex-col border-2 backpeepee p-5 m-10 rounded-3xl drop-shadow-2xl">
-    <h1 class="text-2xl float-left">Question:</h1>
-    <br />
-    <img src={test} alt="question" class="rounded-xl question" />
-    <br />
-    <div
-      class="flex w-3/4 space-x-4 m-auto object-center text-center
-      content-center flex-row">
-      <div class="flex flex-col ansl">
-        <h1 class="text-xl text-center">A</h1>
-        <button>
-          <div class="ans" />
-        </button>
-      </div>
-      <div class="flex flex-col ansl">
-        <h1 class="text-xl text-center">B</h1>
-        <button >
-          <div class="ans" />
-        </button>
-      </div>
-      <div class="flex flex-col ansl">
-        <h1 class="text-xl text-center">C</h1>
-        <button>
-          <div class="ans" />
-        </button>
-      </div>
-      <div class="flex flex-col ansl">
-        <h1 class="text-xl text-center">D</h1>
-        <button>
-          <div class="ans" />
-        </button>
-      </div>
-    </div>
-    <br />
-  </div>
-</div>
